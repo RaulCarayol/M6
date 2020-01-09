38ca7df729dfd2fe8bb1b0c2745a02217336514b
Alumne <Alumne@104-PC03>
2020-01-09 17:34:27 +0100

    commit final

--- M6_UF1_Actividad1/src/VeureInfo.java
+++ M6_UF1_Actividad1/src/VeureInfo.java
@@ -1,43 +1,48 @@
 import java.io.File;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 
 public class VeureInfo {
 	public static void main(String[] args) throws ParseException {
-
-		System.out.println("INFORMACIÓ SOBRE EL FITXER");
-		 File f = new File(args[0]);
-		 if(f.exists()){
-			 if(f.isDirectory()){
-				 String[] arxius = f.list();
-				 for (int i = 0; i<arxius.length; i++){
-					 System.out.println(arxius[i]);
+		//comprueba si se ha especificado la ruta
+		if(args.length==0 || args.equals(null)){
+			System.out.println("No se ha especificado el fichero");
+		}else{
+			 File f = new File(args[0]);
+			 if(f.exists()){
+				 if(f.isDirectory()){
+					 System.out.println("INFORMACIÓ SOBRE EL DIRECTORI");
+					 String[] arxius = f.list();
+					 for (int i = 0; i<arxius.length; i++){
+						 System.out.println(arxius[i]);
+					 }
+				 }else{
+					 System.out.println("INFORMACIÓ SOBRE EL FITXER");
+					 //muestra el nombre
+					 System.out.println("Nom del fitxer : "+f.getName());
+					 //la ruta
+					 System.out.println("Ruta           : "+f.getPath());
+					 //ruta absoluta
+					 System.out.println("Ruta absoluta  : "+f.getAbsolutePath());
+					 //si el archivo se puede escribir
+					 System.out.println("Es pot escriure: "+f.canRead());
+					 //si el archivo se puede leer
+					 System.out.println("Es pot llegir  : "+f.canWrite());
+					 //el tamaño del archivo
+					 System.out.println("Grandaria      : "+f.length());
+					 //devuelve un boolean si esta oculto o no
+					 if(f.isHidden()){
+						 System.out.println("El archivo esta oculto");
+					 }
+					 //mira la que la diferencia en milisegundos de cuando fue modificado y 
+					 //la fecha actual sea de 3 dias en milisegundos	
+					 if(f.lastModified() - System.currentTimeMillis() >=  -259200000){
+					  		System.out.println("El Archivo ha sido modificado hace 3 dias");
+					 }
 				 }
+				
 			 }else{
-				 System.out.println("Nom del fitxer : "+f.getName());
-				 System.out.println("Ruta           : "+f.getPath());
-				 System.out.println("Ruta absoluta  : "+f.getAbsolutePath());
-				 System.out.println("Es pot escriure: "+f.canRead());
-				 System.out.println("Es pot llegir  : "+f.canWrite());
-				 System.out.println("Grandaria      : "+f.length());
-				 if(f.isHidden()){
-					 System.out.println("El archivo esta oculto");
-				 }
-//				 SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-//				  	Date date1 = sdf.parse(sdf.format(f.lastModified()));
-//				  	Date date2
-				  	if(f.lastModified() - System.currentTimeMillis() <=  259200000){
-				  		System.out.println("El Archivo ha sido modificado hace 3 dias");
-				  	}
-				 //System.out.println("Ultima modificacion "+sdf.format(f.lastModified()));
+				 System.out.println("No existe el fichero");
 			 }
-			
-		 }else{
-			 System.out.println("No existe el fichero");
-		 }
+		}
 	}
-
 }
-
