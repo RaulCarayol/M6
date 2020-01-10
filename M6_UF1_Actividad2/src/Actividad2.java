ae3c88ed8564d5020bd1c3e9694e58e58c37166a
Alumne <Alumne@104-PC03>
2020-01-10 19:34:01 +0100

    commit

--- M6_UF1_Actividad2/src/Actividad2.java
+++ M6_UF1_Actividad2/src/Actividad2.java
@@ -19,15 +19,9 @@ public class Actividad2 {
 			System.out.println("Quieres (0)Escribir, (1)leer un coche, (2)Salir ?");
 			 opcion=teclado.nextInt() ;
 			if(opcion == 0){
-				System.out.println("Quieres escribir (1)Un coche o (0)Varios cotxes");
-				if(teclado.nextInt()==1){
-					teclado.nextLine();
-					escribirUnCotxe(teclado);
-				}else{
 					teclado.nextLine();
 					escribirBloqueCotxe(teclado);
-				}
-				
+
 			}else if(opcion == 1){
 				System.out.println("Quieres leer (1)todos los coches o (0)buscar coches por parametro");
 				if(teclado.nextInt()==1){
@@ -42,57 +36,46 @@ public class Actividad2 {
 		teclado.close();
 	
 	}
-	public static void escribirUnCotxe(Scanner teclado) throws IOException{
-		Cotxe cotxe;
-		//Declaració del fitxer
-		System.out.println("Escriba el fichero");
-		//"/home/user/Escriptori/comarquesObject.txt"
-		File fitxer = new File(teclado.nextLine());
-		if(!fitxer.exists()){
-			System.out.println("El fichero no existe, se va ha crear");
-			if(fitxer.createNewFile()){
-				System.out.println(fitxer.getName()+" Creado Correctamente");
-			}else{
-				System.out.println("No se ha Creado el fichero");
-			}
-		}
-		//Crea el flux de sortida
-		FileOutputStream fileout = new FileOutputStream(fitxer,true);
-		//Connectar el flux de bytes al flux de dades
-		ObjectOutputStream dataOuComarq = new ObjectOutputStream(fileout);
-		//datos para el cotxe
-		cotxe = cotxe = pedirDatosCotxe( teclado);
-		dataOuComarq.writeObject(cotxe);//L'escriu al fixer
-		dataOuComarq.close();//Tanca el stream de sortida
-	}
 
 	public static void escribirBloqueCotxe(Scanner teclado) throws IOException{
+		boolean continuar = true;
 		Cotxe cotxe;
 		//Declaració del fitxer
 		System.out.println("Escriba el fichero");
-		//"/home/user/Escriptori/comarquesObject.txt"
 		File fitxer = new File(teclado.nextLine());
 		if(!fitxer.exists()){
-			System.out.println("El fichero no existe, se va ha crear");
-			if(fitxer.createNewFile()){
-				System.out.println(fitxer.getName()+" Creado Correctamente");
+			System.out.println("El fichero no existe");
+			System.out.println("Quieres crear el fichero ??(si/no)");
+			if(teclado.nextLine().equalsIgnoreCase("si")){
+				if(fitxer.createNewFile()){
+					System.out.println(fitxer.getName()+" Creado Correctamente");
+				}else{
+					System.out.println("No se ha Creado el fichero");
+				}
 			}else{
-				System.out.println("No se ha Creado el fichero");
+				continuar = false;
 			}
 		}
-		//Crea el flux de sortida
-		FileOutputStream fileout = new FileOutputStream(fitxer,true);
-		//Connectar el flux de bytes al flux de dades
-		ObjectOutputStream dataOuComarq = new ObjectOutputStream(fileout);
-		do{
-			//datos para el cotxe
-			cotxe = pedirDatosCotxe( teclado);
-			System.out.println("Modelo: " +cotxe.getModel()+ " Marca: "
-					+ cotxe.getMarca()+" Matricula:"+cotxe.getMatricula() +" Año:"+cotxe.getAny());
-			dataOuComarq.writeObject(cotxe);//L'escriu al fixer
-			System.out.println("Quieres continuar?(no/si)");
-		}while(teclado.nextLine().equalsIgnoreCase("si"));
-		dataOuComarq.close();//Tanca el stream de sortida
+		if(continuar){
+			FileOutputStream fileout= new FileOutputStream(fitxer,true);
+
+			//Crea el flux de sortida
+			
+			//Connectar el flux de bytes al flux de dades
+			ObjectOutputStream dataOuComarq = new ObjectOutputStream(fileout);
+			do{
+				//datos para el cotxe
+				cotxe = pedirDatosCotxe( teclado);
+				System.out.println("Modelo: " +cotxe.getModel()+ " Marca: "
+						+ cotxe.getMarca()+" Matricula:"+cotxe.getMatricula() +" Año:"+cotxe.getAny());
+				dataOuComarq.writeObject(cotxe);//L'escriu al fixer
+				System.out.println("Quieres continuar escribiendo coches?(no/si)");
+			}while(teclado.nextLine().equalsIgnoreCase("si"));
+			dataOuComarq.reset();
+			//dataOuComarq.close();//Tanca el stream de sortida
+			System.gc();
+			
+		}
 	}
 	public static Cotxe pedirDatosCotxe(Scanner teclado){
 		String marca,model, matricula;
