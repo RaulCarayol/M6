fbbf505fff525dd9a4d2829ce2da33d27abf8144
Alumne <Alumne@104-PC03>
2020-01-23 17:45:13 +0100

    commit

--- M6_UF1_Actividad5/src/Actividad5.java
+++ M6_UF1_Actividad5/src/Actividad5.java
@@ -1,14 +1,18 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -25,13 +29,7 @@ public class Actividad5 {
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(file);
 		do {
-			System.out.println("---MENU---");
-			System.out.println("0: leer ficehro");
-			System.out.println("1: añadir elemento");
-			System.out.println("2: modificar elemento");
-			System.out.println("3: eliminar elemento");
-			System.out.println("4: Salir");
-			opcion = teclado.nextInt();
+			opcion = mostararMenu(teclado);
 			if(opcion != 4){
 				if(opcion == 0){
 					System.out.println( doc.getDocumentElement().getNodeName());
@@ -40,21 +38,85 @@ public class Actividad5 {
 				}else if(opcion == 1){
 					añadirNodo(doc,teclado,file);
 				}else if(opcion == 2){
-					
+					modificarNodo(doc,teclado,file);
 				}else{
-					eliminarNodo(doc,teclado);
+					eliminarNodo(doc,teclado,file);
 				}
 			}
 		} while (opcion != 4);
 	}
-	private static void eliminarNodo(Document doc, Scanner teclado) {
+	private static void modificarNodo(Document doc, Scanner teclado, File file) throws TransformerConfigurationException, TransformerFactoryConfigurationError, TransformerException {
+		Element elem = buscarElementoId(teclado, doc);
+		System.out.println("que quieres cambiar ?");
+		System.out.println("0: id");
+		System.out.println("1: nom");
+		System.out.println("2: cognom");
+		System.out.println("3: cognom2");
+		System.out.println("4: notaFinal");
+		int opcion = teclado.nextInt();
+		if(opcion == 0){
+			System.out.println("diga el id del alumno");
+			int id = teclado.nextInt();
+			elem.getAttributes().getNamedItem("id").setTextContent(Integer.toString(id));;
+		}else if(opcion == 1){
+			System.out.println("diga el nombre");
+			String nombre = teclado.next();
+			elem.getChildNodes().item(0).getChildNodes().item(0).setTextContent(nombre);
+		}else if(opcion == 2){
+			System.out.println("Diga el primer apellido");
+			String cognom1 = teclado.next();
+			elem.getChildNodes().item(1).getChildNodes().item(0).setTextContent(cognom1);
+		}else if(opcion == 3){
+			System.out.println("Diga el segundo apellido");
+			String cognom2 = teclado.next();
+			elem.getChildNodes().item(2).getChildNodes().item(0).setTextContent(cognom2);
+		}else{
+			System.out.println("Diga la nota final");
+			double notaFinal = teclado.nextDouble();
+			elem.getChildNodes().item(3).getChildNodes().item(0).setTextContent(Double.toString(notaFinal));
+		}
+		guardarFichero(doc, teclado, file);
+	}
+	private static int mostararMenu(Scanner teclado) {
+		int opcion;
+		System.out.println();
+		System.out.println("---MENU---");
+		System.out.println("0: leer ficehro");
+		System.out.println("1: añadir elemento");
+		System.out.println("2: modificar elemento");
+		System.out.println("3: eliminar elemento");
+		System.out.println("4: Salir");
+		opcion = teclado.nextInt();
+		return opcion;
+	}
+	private static void eliminarNodo(Document doc, Scanner teclado,File file) throws TransformerConfigurationException, TransformerFactoryConfigurationError, TransformerException {
+		Element elemento = buscarElementoId(teclado,doc);
+		if(elemento != null){
+			atriubutosNodo(elemento, "");
+			elemento.getParentNode().removeChild(elemento);
+			//guardarFichero(doc, teclado, file);
+		}
+		
+	}
+	private static Element buscarElementoId(Scanner teclado,Document doc){
+		Element elem = null;
 		System.out.println("diga la id del nodo a eliminar");
 		int id = teclado.nextInt();
-		Element elemento = doc.getElementById(Integer.toString(id));
-		if(elemento != null){		
-			System.out.println(elemento.getNodeName());
+		// Get the main element by tag name
+		Node alumnes = doc.getFirstChild();
+		//get a NodeList
+		NodeList list = alumnes.getChildNodes();
+		for (int i = 0; i < list.getLength(); i++) {
+		    Node node = list.item(i);
+		    System.out.println("\nCurrent Element :" + node.getNodeName());
+		    if (node.getNodeType() == Node.ELEMENT_NODE) {
+		        Element eElement = (Element) node;
+		        if(eElement.getAttribute("id").equalsIgnoreCase(Integer.toString(id))){
+		        	elem = eElement;
+		        }
+		    }
 		}
-		//doc.removeChild(elemento);
+		return elem;
 	}
 	private static void añadirNodo(Document doc,Scanner teclado,File file) throws TransformerException {
 		System.out.println("diga el id del alumno");
@@ -66,8 +128,12 @@ public class Actividad5 {
 		System.out.println("Diga el segundo apellido");
 		String cognom2 = teclado.next();
 		System.out.println("Diga la nota final");
-		int notaFinal = teclado.nextInt();
-		System.out.println("Desea guardar los cambios ??(si/no)");
+		double notaFinal = teclado.nextDouble();
+		crearAlumno(doc, id, nombre, cognom1, cognom2, notaFinal);
+		guardarFichero(doc, teclado, file);
+	}
+	private static void crearAlumno(Document doc, int id, String nombre,
+			String cognom1, String cognom2, double notaFinal) {
 		//nodo raiz
 		Node alumnes = doc.getFirstChild();
 		//elemento nuevo
@@ -89,8 +155,13 @@ public class Actividad5 {
 		//notafinal
 		Element nota = doc.createElement("notaFinal");
 		alumno.appendChild(nota);
-		nota.appendChild(doc.createTextNode(Integer.toString(notaFinal)));
-		
+		nota.appendChild(doc.createTextNode(Double.toString(notaFinal)));
+	}
+	
+	private static void guardarFichero(Document doc, Scanner teclado, File file)
+			throws TransformerFactoryConfigurationError,
+			TransformerConfigurationException, TransformerException {
+		System.out.println("Desea guardar los cambios ??(si/no)");
 		if(teclado.next().equals("si")){
 			 TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			  Transformer transformer = transformerFactory.newTransformer();
@@ -99,6 +170,7 @@ public class Actividad5 {
 			  transformer.transform(source, result);
 		}
 	}
+	
 	public static void atriubutosNodo(Element e,String espacio){
 		//se obtiene los nodos hijos
 		NodeList nodeList = e.getChildNodes();
