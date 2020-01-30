764ea14a69e259b8794b56cab3e19cf2ec451ca9
Alumne <Alumne@104-PC03>
2020-01-30 18:11:15 +0100

    commit

--- M6_UF1_Actividad6/src/Plantas.java
+++ M6_UF1_Actividad6/src/Plantas.java
@@ -1,10 +1,11 @@
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+@XmlRootElement(name="Plantas")
 public class Plantas {
 	private Planta[] planta;
-
+	@XmlElement(name="Planta")
 	public Planta[] getPlantas() {
 		return planta;
 	}
