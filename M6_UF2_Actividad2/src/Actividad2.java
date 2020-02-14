6827e6bd32f142fcdd04bc96d964ecf53e9e7e61
Alumne <Alumne@104-PC03>
2020-02-14 17:20:09 +0100

    commit

--- M6_UF2_Actividad2/src/Actividad2.java
+++ M6_UF2_Actividad2/src/Actividad2.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,16 +16,75 @@ public class Actividad2 {
 		 graficos = new M6_Actividad2_Graphics();
 		graficos.btnInsertar.addActionListener(new ActionListener() {
 		 	public void actionPerformed(ActionEvent arg0) {
-		 		realizarTransaccion(connection);
+		 		connection = realizarConexion();
+					realizarTransaccion(connection);
+					//cerrar conexion
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+				
 		 	}
 
 			private void realizarTransaccion(Connection connection) {
-				// TODO Auto-generated method stub
-				
+				PreparedStatement preparedStatement = null;
+				try {
+					//realizar conexion si esta cerrada
+					if(connection.isClosed()){connection = realizarConexion();}
+					//quitar el autoCommit
+					connection.setAutoCommit(false);
+					//realizar el insert
+					preparedStatement = connection.prepareStatement("INSERT INTO Poblaciones (nombre, CP) VALUES (?,?)");
+					//poner los datos para evitar injecciones
+					preparedStatement.setInt(1, 43203);
+					preparedStatement.setString(2, "Tarragona");
+					preparedStatement.executeUpdate();
+					//realizar el segundo insert
+					preparedStatement = connection.prepareStatement("INSERT INTO Poblaciones (nombre, CP) VALUES (?,?)");
+					//poner los datos para evitar injecciones
+					preparedStatement.setInt(1, 43217);
+					preparedStatement.setString(2, "Barcelona");
+					preparedStatement.executeUpdate();
+
+					Thread.sleep(4000);
+					//realizar delete
+					preparedStatement = connection.prepareStatement("DELETE FROM Poblaciones WHERE CP = ?");
+					preparedStatement.setInt(1, 43203);
+					preparedStatement.executeUpdate();
+					//hacer un commit
+					connection.commit();
+					connection.setAutoCommit(true);
+					graficos.lblMensaje.setText("Insertado y borrado.");
+					
+				//recojer las excepciones
+				} catch (SQLException e) {
+					try {
+						//hacer un rollback en caso de una excepcion sql
+						connection.rollback();
+					} catch (SQLException e1) {
+						//mostrar mensje si salta una excepcion durante el rollback
+						graficos.lblMensaje.setText("El rollback ha fallado");
+					}
+					//mostrar cuando ha acabado 
+					graficos.lblMensaje.setText("Realizado Rollback");
+
+				} catch (InterruptedException e) {
+					//Mostrar mensaje de error
+					graficos.lblMensaje.setText(e.getMessage());
+
+				} finally {
+					try {
+						if (preparedStatement != null && !preparedStatement.isClosed()) {
+							preparedStatement.close();
+						}
+					} catch (SQLException e) {
+						graficos.lblMensaje.setText("Error cerrando Prepared Statement");
+					}
+				}
 			}
 		 });
-		connection = realizarConexion();
-		
 	}
 	
 	private static Connection realizarConexion() {
--- M6_UF2_Actividad2/src/M6_Actividad2_Graphics.java
+++ M6_UF2_Actividad2/src/M6_Actividad2_Graphics.java
@@ -12,61 +12,58 @@ import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 
 public class M6_Actividad2_Graphics extends JFrame {
 	public JButton btnInsertar;
+	public JLabel lblMensaje;
 	private JPanel contentPane;
 	private final JPanel panel_1 = new JPanel();
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					M6_Actividad2_Graphics frame = new M6_Actividad2_Graphics();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
 	public M6_Actividad2_Graphics() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 400, 250);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(6, 1, 0, -2));
-		
+
 		JPanel panel = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-		flowLayout.setHgap(25);
-		flowLayout.setVgap(15);
 		contentPane.add(panel);
-		
+		panel.setLayout(new GridLayout(0, 2, 0, 0));
+
 		JLabel lblCP = new JLabel("Codigo Postal");
 		panel.add(lblCP);
-		
+
 		JLabel lblNombrePobalacion = new JLabel("Nombre Poblaci\u00F3n");
 		panel.add(lblNombrePobalacion);
-		FlowLayout flowLayout_1 = (FlowLayout) panel_1.getLayout();
-		flowLayout_1.setHgap(25);
-		flowLayout_1.setVgap(0);
 		contentPane.add(panel_1);
-		
+		panel_1.setLayout(new GridLayout(2, 2, 0, 0));
+
 		JLabel lblCodi = new JLabel("43203");
 		panel_1.add(lblCodi);
-		
+
 		JLabel lblNom = new JLabel("Tarragona");
 		panel_1.add(lblNom);
 		
+		JLabel label = new JLabel("43217");
+		panel_1.add(label);
+		
+		JLabel lblBarcelona = new JLabel("Barcelona");
+		panel_1.add(lblBarcelona);
+
 		JPanel panel_2 = new JPanel();
 		contentPane.add(panel_2);
-		
-		 btnInsertar = new JButton("Insertar");
+
+		btnInsertar = new JButton("Insertar");
 		panel_2.add(btnInsertar);
+
+		lblMensaje = new JLabel("");
+		lblMensaje.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblMensaje);
+
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 400, 250);
 	}
 
 }
