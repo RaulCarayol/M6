f1adf01839a4690cfbcc5b3ef49ec228d6c92adc
Raul <raul.carayol@insbaixcamp.cat>
2020-05-10 19:46:37 +0200

    commit final

--- M6_UF3_Actividad3/src/main/java/m6_uf3_actividad3/M6_Uf3_Acticividad3.java
+++ M6_UF3_Actividad3/src/main/java/m6_uf3_actividad3/M6_Uf3_Acticividad3.java
@@ -48,10 +48,34 @@ public class M6_Uf3_Acticividad3 {
             System.out.println("*** LA COL·LECCIÓ NO EXISTEIX ***");
         }
 	XPathQueryService servei =(XPathQueryService) col.getService("XPathQueryService", "1.0");
-        
-        ResourceSet result =
-		servei.query(buscarEmpleadoDeUnDepartamento(teclado));
-		
+        int opcion=0;
+        while(opcion != 4){
+            System.out.println("Diga una opcion:");
+            System.out.println("1: Mostrar los empleados de un departamento");
+            System.out.println("2: insertar un departamento");
+            System.out.println("3: eliminar un departamento");
+            System.out.println("4: Salir");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            if(opcion ==1){
+               buscarEmpleadoDeUnDepartamento(teclado,servei);
+
+            }else if(opcion==2){
+                insereixdep(teclado,servei);
+            }else if(opcion==3){
+                esborradep(teclado,servei);
+            }
+        }
+        //S'esborra
+	col.close();
+    }
+    public static void buscarEmpleadoDeUnDepartamento(Scanner teclado,XPathQueryService servei) throws XMLDBException{
+        System.out.println("Diga el nombre del departamento");
+        String departamento = teclado.next();
+        String sentencia = "for $num in /departamentos/DEP_ROW[DNOMBRE='"+departamento+"']/DEPT_NO"
+                + " let $emp := /EMPLEADOS/EMP_ROW[DEPT_NO=$num] return $emp" ;
+                ResourceSet result =
+		servei.query(sentencia);
 		//Recórrer les dades del recurs
 		ResourceIterator i;
 		i = result.getIterator();
@@ -62,17 +86,54 @@ public class M6_Uf3_Acticividad3 {
 			System.out.println((String)r.getContent());
 		
 		}
-		
-
-     
-        //S'esborra
-	col.close();
     }
-    public static String buscarEmpleadoDeUnDepartamento(Scanner teclado){
-        System.out.println("Diga el nombre del departamento");
+
+    private static void insereixdep(Scanner teclado,XPathQueryService servei) throws XMLDBException {
+         System.out.println("Diga el nombre del departamento");
         String departamento = teclado.next();
-        return "for $num in /departamentos/DEP_ROW[DNOMBRE='"+departamento+"']/DEPT_NO"
-                + " let $emp := /EMPLEADOS/EMP_ROW[DEPT_NO=$num] return $emp" ;
+         System.out.println("Diga el numero del departamento");
+        int num = teclado.nextInt();
+         System.out.println("Diga la localizacion del departamento");
+        String loc = teclado.next();
+        
+             ResourceSet result =
+		servei.query("update insert\n" +"<DEP_ROW><DEPT_NO>"+num+"</DEPT_NO>"
+                        + "<DNOMBRE>"+departamento+"</DNOMBRE>"
+                        + "<LOC>"+loc+"</LOC></DEP_ROW> into /departamentos");
+
+
+                System.out.println("Se ha insertado con exito");
+                mostrarDepartamentos(servei);
+    }
+
+    private static void esborradep(Scanner teclado,XPathQueryService servei) throws XMLDBException {
+        //mostrar todos los departamentos
+        mostrarDepartamentos(servei);
+
+         System.out.println("Diga el numero del departamento");
+        int num = teclado.nextInt();
+
+        
+             ResourceSet result =
+		servei.query("update delete /departamentos/DEP_ROW[DEPT_NO="+num+"]");
+
+             
+                System.out.println("Se ha Borrado con exito");
+                mostrarDepartamentos(servei);
     }
     
+    private static void mostrarDepartamentos(XPathQueryService servei) throws XMLDBException{
+                ResourceSet result0 = servei.query("for $dep in /departamentos/DEP_ROW\n" 
+                +"return $dep");
+        //Recórrer les dades del recurs
+		ResourceIterator i;
+		i = result0.getIterator();
+		if (!i.hasMoreResources())
+			System.out.println("LA CONSULTA NO TORNA RES");
+		while (i.hasMoreResources()) {
+			Resource r = i.nextResource();
+			System.out.println((String)r.getContent());
+		
+		}
+    }
 }
